ae3565ac7129badb964a949f98785db66a34645c
Shini <halimshini@hotmail.com>
2023-04-09 01:00:07 -0400

    Added  model for users,thougts

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require('mongoose');
+
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim:true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: () => Promise.resolve(false),
+        message: 'Email validation failed'
+      }
+    },
+    thoughts:
+    [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id:false,
+  }
+);
+
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  });
+
+const User = model('user', userSchema);
+
+module.exports = User;
