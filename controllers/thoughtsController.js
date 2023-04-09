edc8d738cbb2ed32b85b6c83d551068cacfddb9f
Shini <halimshini@hotmail.com>
2023-04-09 01:02:45 -0400

    added userController and thoughtsController

---
+++ controllers/thoughtsController.js
@@ -0,0 +1,51 @@
+module.exports = {
+    //get all thoughts
+    getThoughts(req, res) {
+        Thought.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
+    //get by id
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .select('-__v')
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    //post
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then((answer) => res.json(answer))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    //update 
+    updateThought(req, res) {
+        Thought.findOneAndUpdate({ _id: req.params.thoughtId })
+            .then((answer) => res.json(answer))
+            .catch((err) => res.status(500).json(err));
+    },
+    //Delete
+    removeThought(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+            .then((answer) => res.json(answer))
+            .catch((err) => res.status(500).json(err))
+    },
+    //create a reaction
+    createReaction(req, res) {
+        Thought.create({ _id: req.params.reactionId })
+            .then((answer) => res.json(answer))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    //remove  reaction
+    removeReaction(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.reactionId })
+            .then((answer) => res.json(answer))
+            .catch((err) => res.status(500).json(err))
+    },
+}
---
+++ controllers/userController.js
@@ -0,0 +1,51 @@
+module.exports = {
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((answer) => res.json(answer))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  //update user
+  updateUser(req, res) {
+    User.findOneAndUpdate({_id: req.params.userId})
+      .then((answer) => res.json(answer))
+      .catch((err) => res.status(500).json(err));
+  },
+  //delete user
+
+  removeUser(req,res){
+    User.findOneAndDelete({_id:req.params.userId})
+    .then((answer)=>res.json(answer))
+    .catch((err)=> res.status(500).json(err))
+  },
+    //Create Friend
+  addFriend(req, res) {
+    User.create({_id:req.params.userId})
+      .then((answer) => res.json(answer))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  //remove Friend
+  removeFriend(req,res){
+    User.findOneAndDelete({_id:req.params.userId})
+    .then((answer)=>res.json(answer))
+    .catch((err)=> res.status(500).json(err))
+  },
+};
+
